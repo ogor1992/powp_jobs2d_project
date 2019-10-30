be3bde71f86e33941be42bb78c0aa81835c5c2ea
ogor1992 <ogor1992@gmail.com>
2019-10-30 11:45:30 +0100

    initial changes in lab2 - command

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,6 @@
+package edu.kis.powp.command;
+
+public class OperateToCommand {
+    private int X = 0, Y = 0;
+}
+
---
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -0,0 +1,5 @@
+package edu.kis.powp.command;
+
+public class SetPositionCommand {
+    private int X = 0, Y = 0;
+}
